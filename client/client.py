9049b6bc3d334187fd120e76231eb400f65e0804
odh1995 <dinghan1995@yahoo.com>
2023-08-05 13:32:23 +0800

    added rest example

--- client/client.py
+++ client/client.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, File, UploadFile
+import time
 import grpc
+import requests
 
 import file_transfer_pb2
 import file_transfer_pb2_grpc
@@ -12,11 +14,31 @@ def upload_file_with_grpc(file_content):
         response = stub.UploadFile(iter([file_transfer_pb2.FileChunk(content=file_content)]))
         return response.success
 
-@app.post("/upload/")
-async def upload_file(file: UploadFile = File(...)):
+@app.post("/upload_grpc/")
+async def upload_file_grpc(file: UploadFile = File(...)):
     content = await file.read()
+    start_time = time.time()
     success = upload_file_with_grpc(content)
     if success:
+        print(time.time() - start_time)
         return {"message": f"File {file.filename} has been uploaded."}
     else:
         return {"message": "Failed to upload the file."}
+
+@app.post("/upload_rest")
+async def upload_file_rest(file: UploadFile = File(...)):
+    server_url = "http://localhost:8001/upload/"
+    headers = {"accept": "application/json"}  # Set the Content-Type header for file upload
+    files = {"file": (file.filename, await file.read())}
+    start_time = time.time()
+    response = requests.post(server_url, files=files, headers=headers)
+    if response.status_code == 200:
+        print(time.time() - start_time)
+        return response.json()
+    else:
+        print(response)
+        return {"message": "Failed to upload the file."}
+
+@app.post("/send_text_rest")
+async def send_text_rest(text: str):
+    pass
---
+++ rest_server/server.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, File, UploadFile
+import shutil
+from pathlib import Path
+
+app = FastAPI()
+
+@app.post("/upload/")
+async def upload_file(file: UploadFile = File(...)):
+    with open("uploaded_file.txt", "wb") as f:
+        while chunk := await file.read(1024):
+            f.write(chunk)
+    return {"success": True}
